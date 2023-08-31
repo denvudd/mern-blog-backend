639876952144e1c3f547f5d0ed7e47612620f95f
Dima <dimontv7@gmail.com>
2023-08-31 22:48:35 +0300

    create auth/login API route

--- index.js
+++ index.js
@@ -23,6 +23,47 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Something went wrong... Please, try again later.", // user not found
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(404).json({
+        success: false,
+        message: "Something went wrong... Please, try again later.", // invalid password
+      });
+    }
+
+    const token = jwt.sign({ _id: user._id }, "secret", { expiresIn: "30d" });
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      success: true,
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong... Please, try again later.",
+    });
+  }
+});
+
 app.post("/auth/register", registerValidator, async (req, res) => {
   try {
     const errors = validationResult(req);
